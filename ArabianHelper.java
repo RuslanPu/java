1b2d599f216c474dccfc2454883274c1ee47bfd6
Ruslan <ruslanpunin@yande.ru>
2019-10-06 00:10:09 +0300

    Add project

---
+++ ArabianHelper.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io;
+
+public class ArabianHelper {
+    public static int romanToDecimal(java.lang.String romanNumber) {
+        int decimal = 0;
+        int lastNumber = 0;
+        String romanNumeral = romanNumber.toUpperCase();
+        for (int x = romanNumeral.length() - 1; x >= 0 ; x--) {
+            char convertToDecimal = romanNumeral.charAt(x);
+            switch (convertToDecimal) {
+                case 'X':
+                    decimal = processDecimal(10, lastNumber, decimal);
+                    lastNumber = 10;
+                    break;
+
+                case 'V':
+                    decimal = processDecimal(5, lastNumber, decimal);
+                    lastNumber = 5;
+                    break;
+
+                case 'I':
+                    decimal = processDecimal(1, lastNumber, decimal);
+                    lastNumber = 1;
+                    break;
+            }
+        }
+        return decimal;
+    }
+
+    private static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal) {
+            return lastDecimal - decimal;
+        } else {
+            return lastDecimal + decimal;
+        }
+    }
+}
